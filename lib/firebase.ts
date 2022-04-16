e8991d87d051b1d1b539b831d105640df9952452
patrick <patrickgkenworthy@gmail.com>
2022-04-15 23:10:28 -0600

    Added user profile page

--- lib/firebase.ts
+++ lib/firebase.ts
@@ -1,6 +1,7 @@
 import { initializeApp, getApp, FirebaseApp } from 'firebase/app'
 import { Auth, getAuth, GoogleAuthProvider } from 'firebase/auth'
-import { getFirestore, Firestore } from 'firebase/firestore'
+import { getFirestore,Firestore, collection, query,
+    where, limit,getDocs, QueryDocumentSnapshot, DocumentData, orderBy } from 'firebase/firestore'
 import { getStorage, FirebaseStorage } from 'firebase/storage'
 
 const firebaseConfig = {
@@ -25,3 +26,44 @@ export const auth: Auth = getAuth()
 export const googleAuthProvider: GoogleAuthProvider = new GoogleAuthProvider()
 export const firestore: Firestore = getFirestore()
 export const storage: FirebaseStorage = getStorage()
+
+/// Helper functions
+
+/**
+ * Gets a users/{uid} document with username
+ * @param {string} username
+*/
+export async function getUserWithUsername(username: string) {
+    const usersRef = collection(firestore, 'users')
+    const usersQuery = query(usersRef,
+                        where('username', '==', username),
+                        limit(1))
+    const userDoc = (await getDocs(usersQuery)).docs[0]
+    return userDoc
+}
+
+/**
+ * Gets all users/{uid}/posts documents from user
+ * @param {QueryDocumentSnapshot<DocumentData>} userDoc
+*/
+export async function getPostsFromUser(userDoc : QueryDocumentSnapshot<DocumentData>) {
+    const postsRef = collection(userDoc.ref, 'posts')
+    const postsQuery = query(postsRef,
+                        where('published', '==', true),
+                        limit(5),
+                        orderBy('createdAt','desc'))
+    const posts = (await getDocs(postsQuery)).docs
+    return posts
+}
+
+/**
+ * Converts a firestore document to JSON
+ */
+export function postToJSON(doc){
+    const data = doc.data();
+    return {
+        ...data,
+        createdAt: data.createdAt.toMillis(),
+        updatedAt: data.updatedAt.toMillis()
+    }
+}
--- pages/[username]/index.tsx
+++ pages/[username]/index.tsx
@@ -1,6 +1,33 @@
-export default function Page({ }) {
+import UserProfile from '../../components/UserProfile'
+import PostFeed from '../../components/PostFeed'
+import { getUserWithUsername, getPostsFromUser, postToJSON } from '../../lib/firebase';
+
+export async function getServerSideProps({ query }){
+  
+  const { username } = query;
+
+  let user = null
+  let posts = null
+
+  const userDoc = await getUserWithUsername(username)
+  if (userDoc) {
+    user = userDoc.data()
+    const postsDocs = await getPostsFromUser(userDoc)
+    if (postsDocs) {
+      posts = postsDocs.map(postToJSON)
+    }
+  }
+
+  return {
+    props: { user, posts }
+  }
+}
+
+export default function UserProfilePage({ user, posts }) {
   return (
     <main>
+      <UserProfile user={user} />
+      <PostFeed posts={posts} />
     </main>
   )
 }
